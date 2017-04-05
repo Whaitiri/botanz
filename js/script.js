89d3345b852835bb76b50cc203f2cf50c1406812
Omri Webber <845868822@kent-202-10m.local>
2017-04-06 10:35:51 +1200

    Added in Contact Us and Slideshow

--- js/script.js
+++ js/script.js
@@ -33,3 +33,55 @@ $(document).ready(function(){
 	});
 
 });
+
+
+// Slideshow Carousel
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("slide");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 5000); // Change image every 2 seconds
+}
+
+// Modal popup
+
+// Get the modal
+var modal = document.getElementById('contactus-popup');
+
+// Get the button that opens the modal
+var btn = document.getElementById("contactus-btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+});
